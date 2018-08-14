a895ea9c17f22e5ee0be2ac2beaaa941f87ab6e9
eldron <eldron@163.com>
2018-08-14 17:19:23 +0800

    know how to download file

---
+++ test_client.py
@@ -0,0 +1,20 @@
+from socket import *
+from tlslite import TLSConnection
+from tlslite.api import *
+
+sock = socket.socket(AF_INET, SOCK_STREAM)
+sock.connect(('localhost', 4443))
+connection = TLSConnection(sock)
+connection.handshakeClientCert()
+request = "GET /bigger.pcap HTTP/1.0\r\n\r\n"
+connection.send(request)
+cnt = 0
+while True:
+    r = connection.recv(2048)
+    if len(r) > 0:
+        cnt = cnt + len(r)
+        print 'received ' + str(cnt)
+        #print r
+    else:
+        print 'receive file completed'
+        break
